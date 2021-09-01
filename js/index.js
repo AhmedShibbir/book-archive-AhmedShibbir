80502b7d87374640940983bba9465753cead4b93
Shibbir Ahmed <shibbirahmedops@gmail.com>
2021-09-02 02:58:42 +0600

    bootstrap added and nav search menu added

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 /* js connection checking
 console.log("js connected"); */
+const searchBook = () => {
+    const searchingInput = document.getElementById("searchingInput");
+    const searchedBook = searchingInput.value;
+    searchingInput.value = '';
+    console.log(searchedBook);
+}
