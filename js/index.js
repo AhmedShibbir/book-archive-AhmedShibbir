032dfa0dadead29090e130869ebc86c0a6e2612f
Shibbir Ahmed <shibbirahmedops@gmail.com>
2021-09-02 17:09:25 +0600

    result in amount section showing done

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ const searchBook = () => {
     const searchingInput = document.getElementById("searchingInput");
     const searched = searchingInput.value;
     searchingInput.value = '';
+    const resultFoundArea = document.getElementById("resultFoundArea");
+    resultFoundArea.innerHTML = "";
     searchedBook(searched);
 }
 const searchedBook = searched => {
@@ -14,16 +16,26 @@ const searchedBook = searched => {
 const extractData = data => {
     const {numFound} = data;
     const showingData = data.docs.length;
+    const div = document.createElement("div");
+    const header1 = document.createElement("h1");
+    header1.innerText = `Total Results found by name: ${numFound}`;
+    div.appendChild(header1);
+    const header2 = document.createElement("h1");
+    header2.innerText = `Available to show: ${showingData}`;
+    div.appendChild(header2);
+    const resultFoundArea = document.getElementById("resultFoundArea");
+    resultFoundArea.innerHTML = "";
+    resultFoundArea.appendChild(div);
     data.docs.forEach(element => {
         individualBookData(element);
     });
 }
 const individualBookData = element => {
-    // console.log(element);
-    const {title} = element;
+    console.log(element);
+    const {title,cover_i} = element;
+    //author_name,publish_date,publisher are array type
     const author_name = element.author_name;
     const publish_date = element.publish_date;
-    const publisher=element.publisher;
-    console.log(title,author_name,publish_date,publisher);
-    
+    const publisher = element.publisher;
+    // console.log(...author_name);
 }
